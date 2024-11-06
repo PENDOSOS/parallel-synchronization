fb3555c7259b1000c5c09006373c77f70f71e2c7
Ivan <i.puchkin@nicetu.spb.ru>
2024-11-06 21:06:34 +0300

    i dont know what changes i've made here

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,6 @@
 #include "EventQueue/EventQueue.h"
 #include "parallel-executor/ReadLibrary.h"
 #include "parallel-executor/Hierarchy.h"
-#include "parallel-executor/ThreadTasks.h"
 #include "parallel-executor/includes.h"
 
 #include <iostream>
@@ -12,9 +11,6 @@ int main()
 
   std::shared_ptr<Device> deviceA = std::make_shared<DeviceA>();
   std::shared_ptr<Device> deviceB = std::make_shared<DeviceB>();
-
-  //std::function<void(const std::shared_ptr<EventQueue>&, const std::shared_ptr<Device>&)> readFromA = readA;
-  //std::function<void(const std::shared_ptr<EventQueue>&, const std::shared_ptr<Device>&)> readFromB = readB;
 
   std::thread threadA(readA, eventQueue, deviceA);
   std::thread threadB(readB, eventQueue, deviceB);
