1e2ac732ece88be9369866186e453884a80fc293
Ivan <i.puchkin@nicetu.spb.ru>
2024-11-06 21:01:47 +0300

    added endless loop in read functions

--- src/parallel-executor/ReadLibrary.h
+++ src/parallel-executor/ReadLibrary.h
@@ -6,38 +6,44 @@
 
 void readA(const std::shared_ptr<EventQueue>& queue, const std::shared_ptr<Device>& device)
 {
-  auto start = std::chrono::high_resolution_clock::now();
-  
-  queue->push(std::make_shared<const StartedEvent>(device));
-  queue->push(std::make_shared<const DataEvent>(device));
-  
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-
-  while (duration.count() < 5)
+  while (true)
   {
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-  }
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    queue->push(std::make_shared<const StartedEvent>(device));
+    queue->push(std::make_shared<const DataEvent>(device));
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
 
-  queue->push(std::make_shared<const WorkDoneEvent>(device));
+    while (duration.count() < 5)
+    {
+      end = std::chrono::high_resolution_clock::now();
+      duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    }
+
+    queue->push(std::make_shared<const WorkDoneEvent>(device));
+  }
 }
 
 void readB(const std::shared_ptr<EventQueue>& queue, const std::shared_ptr<Device>& device)
 {
-  auto start = std::chrono::high_resolution_clock::now();
-
-  queue->push(std::make_shared<const StartedEvent>(device));
-  queue->push(std::make_shared<const DataEvent>(device));
-  
-  auto end = std::chrono::high_resolution_clock::now();
-  auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-
-  while (duration.count() < 1)
+  while (true)
   {
-    end = std::chrono::high_resolution_clock::now();
-    duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
-  }
+    auto start = std::chrono::high_resolution_clock::now();
 
-  queue->push(std::make_shared<const WorkDoneEvent>(device));
+    queue->push(std::make_shared<const StartedEvent>(device));
+    queue->push(std::make_shared<const DataEvent>(device));
+    
+    auto end = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+
+    while (duration.count() < 1)
+    {
+      end = std::chrono::high_resolution_clock::now();
+      duration = std::chrono::duration_cast<std::chrono::seconds>(end - start);
+    }
+
+    queue->push(std::make_shared<const WorkDoneEvent>(device));
+  }
 }
